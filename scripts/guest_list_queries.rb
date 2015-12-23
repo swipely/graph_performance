438e2cbdd4d6b79878eb722d59b73feb1d6c3a3e
danhodge <dan@swipely.com>
2015-12-23 15:44:17 -0500

    use Titan 1.0 for queries

--- scripts/guest_list_queries.rb
+++ scripts/guest_list_queries.rb
@@ -1,23 +1,33 @@
 start = Time.now
-require 'pacer-titan'
 require 'json'
 require 'java'
 require 'benchmark'
 require 'json'
+require 'lock_jar'
+
+LockJar.load
+require 'java'
+java_import 'com.thinkaurelius.titan.core.TitanFactory'
 
 def query(graph, list_vertex, order_by, direction, limit)
-  list_vtx = graph.blueprints_graph.get_vertex(list_vertex.element_id)
-
   java_import 'com.thinkaurelius.titan.graphdb.query.vertex.VertexCentricQueryBuilder'
-  java_import 'com.tinkerpop.blueprints.Direction'
-  java_import 'com.thinkaurelius.titan.core.Order'
+  java_import 'org.apache.tinkerpop.gremlin.structure.Direction'
+  java_import 'org.apache.tinkerpop.gremlin.process.traversal.Order'
 
   # uses Titan's query builder directly to gain access to the order_by functionality
-  query_builder = VertexCentricQueryBuilder.new(list_vtx)
+  order = case direction
+          when 'DESC'
+            Order.decr
+          when 'ASC'
+            Order.incr
+          else
+            raise "Unknown direction: #{direction}"
+          end
+  query_builder = VertexCentricQueryBuilder.new(list_vertex)
   query_builder
     .labels('members')
     .direction(Direction.value_of('OUT'))
-    .order_by(order_by, Order.value_of(direction))
+    .order_by(order_by, order)
     .limit(limit)
     .edges
 end
@@ -29,10 +39,10 @@ query_by = ARGV[1]
 order = ARGV[2]
 limit = ARGV[3].to_i
 
-graph = Pacer.titan(config_file)
+graph = TitanFactory.open(config_file)
 
-store_vertex = graph.v(label: 'store', store_pretty_url: 'blue-star-cafe-and-pub-seattle').first
-all_guests = store_vertex.out_e(:guest_lists, guest_list_id: 'all').in_v(label: 'store_guest_list').first
+# TODO: figure out how to query for vertex by label
+all_guests = graph.traversal.V.has('store_pretty_url', 'blue-star-cafe-and-pub-seattle').outE('guest_lists').has('guest_list_id', 'all').inV.next
 
 startup_time = ((Time.now - start) * 1000).round(2)
 duration = Benchmark.realtime { query(graph, all_guests, query_by, order, limit).to_a.last }
