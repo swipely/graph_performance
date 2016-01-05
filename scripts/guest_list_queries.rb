da8895e65233e3319cbebb53191a415a9da18ea4
danhodge <dan@swipely.com>
2016-01-05 14:47:47 -0500

    enable profiling

--- scripts/guest_list_queries.rb
+++ scripts/guest_list_queries.rb
@@ -9,7 +9,7 @@ LockJar.load
 require 'java'
 java_import 'com.thinkaurelius.titan.core.TitanFactory'
 
-def query(graph, list_vertex, order_by, direction, limit)
+def query(graph, list_vertex, order_by, direction, limit, proc)
   java_import 'org.apache.tinkerpop.gremlin.process.traversal.Order'
 
   order = case direction
@@ -21,10 +21,49 @@ def query(graph, list_vertex, order_by, direction, limit)
             raise "Unknown direction: #{direction}"
           end
 
-  graph.traversal.V(list_vertex.id).out_e('members').order.by(order_by, order).take(limit)
+  t = graph.traversal.V(list_vertex.id).out_e('members').order.by(order_by, order).limit(limit).profile
+  t.each(&proc)
+  $stderr.puts t.get_side_effects.get('~metrics').to_string
+end
+
+def read_edge
+  proc do |edge|
+    edge.properties.each do |property|
+      property.property_key.name
+      property.value
+    end
+  end
+end
+
+def show_edge
+  proc do |edge|
+    puts edge.properties.map { |property| "#{property.property_key.name} = #{property.value}" }.join(', ')
+  end
+end
+
+def read_vertex_and_edge
+  proc do |edge|
+    # get the guest_id value from the vertex
+    vertex = edge.get_vertex(1)
+    prop = vertex.property('guest_id')
+    if prop.present?
+      prop.value
+      #else
+      #puts "No guest_id for vertex: #{vertex.id}"
+    end
+
+    # get all properties
+    edge.properties.each do |property|
+      property.property_key.name
+      property.value
+    end
+  end
 end
 
 raise ArgumentError, "Usage: #{__FILE__} <config_file> <query_by> <order> <limit>" unless ARGV.length == 4
+
+java_import 'java.lang.System'
+System.set_property('tinkerpop.profiling', 'true')
 
 config_file = ARGV[0]
 query_by = ARGV[1]
@@ -47,23 +86,7 @@ startup_time = ((Time.now - start) * 1000).round(2)
 
 duration = Benchmark.realtime do
   # Note: tried doing this the other way (querying for vertices then retrieving the members edge, but it was much slower)
-  query(graph, all_guests, query_by, order, limit).each do |edge|
-    # get the guest_id value from the vertex
-    vertex = edge.get_vertex(1)
-    prop = vertex.property('guest_id')
-    if prop.present?
-      prop.value
-    #else
-      #puts "No guest_id for vertex: #{vertex.id}"
-    end
-
-    # get all properties
-    #puts edge.properties.map { |property| "#{property.property_key.name} = #{property.value}" }.join(', ')
-    edge.properties.each do |property|
-      property.property_key.name
-      property.value
-    end
-  end
+  query(graph, all_guests, query_by, order, limit, read_edge)
 end
 
 puts({ startup_time_ms: startup_time, query_duration_ms: (duration * 1000).round(2) }.to_json)
